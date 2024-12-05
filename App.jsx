3a625ff77d360571005cf6bb9cbfce7dce4be9af
Dhruv-Sh-git <dhruvshaswat25@gmail.com>
2024-12-05 18:30:16 +0530

    bg_color_changer

--- App.jsx
+++ App.jsx
@@ -1,19 +1,28 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [counter,setcounter]=useState(0)
-  const addvalue=()=>{
-    setcounter(counter + 1)
+  const [color, setcolor] = useState('black')
+  const setcolor1=()=>{
+    setcolor('red')
   }
-  const subvalue=()=>{
-    setcounter(counter - 1)
+  const setcolor2=()=>{
+    setcolor('blue')
   }
+  const setcolor3=()=>{
+    setcolor('green')
+  }
+
   return (
-    <>
-      <h1>COUNTER VALUE {counter}</h1>
-      <button onClick={addvalue}>ADD VALUE</button>
-      <button onClick={subvalue}>REMOVE VALUE</button>
-    </>
+    <div className='w-full h-screen duration-200 fixed flex flex-wrap justify-center items-center m-0 p-0' style={{backgroundColor:color}}> 
+      <div className='fixed flex flex-wrap justify-center items-center space-x-5 bg-white rounded-3xl h-32 w-72'>
+        <button className='bg-red-600 rounded-md h-9 w-12' onClick={setcolor1}>red</button>
+        <button className='bg-blue-600 rounded-md h-9 w-12' onClick={setcolor2}>blue</button>
+        <button className='bg-green-600 rounded-md h-9 w-12' onClick={setcolor3}>green</button>
+      </div>
+    </div>
   )
 }
 
